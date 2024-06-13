6bd17de1a4d7f6e00f5c70e2f64214c397747406
tinsae-ghilay <85639930+tinsae-ghilay@users.noreply.github.com>
2024-06-13 23:26:08 +0200

    almost done / DONE

--- UZ_4/main.cpp
+++ UZ_4/main.cpp
@@ -6,18 +6,64 @@
 
 using namespace std;
 
+
+
+
 int main(){
 
-    Sort sort;
+    // size of array
+    constexpr  int size = 50;
 
-    auto arr = sort.generateArray(10);
-    sort.printVector(arr);
-    sort.selection(arr);
-    sort.printVector(arr);
+    // Suffled array to be sorted
+    auto arr = generateArray(size);
+    // time stamps
+    // loop insertion sort
+    // insertion sort has to copy more times but compare less.
+    // but compying takes more time than comparing ints
+    // hence slower than slection sort. but faster than recursive insertion sort
+    const auto loopIns = testInsertion(10);
+    cout<<endl << "Loop insertion took "<<loopIns << " microseconds"<<endl;
+    // print shuffled vector
+    cout << "shuffling array ..."<<endl<< "Shuffled array";
+    printVector(arr);
+    cout << "loop insertion sorting ..."<<endl << "Sorted array";
+    // recursive insertion sorting
+    loopInsertion(arr);
+    // print sorted vector
+    printVector(arr);
 
-    sort.shuffleVector(arr);
-    sort.printVector(arr);
+    // recursive insertion sort
+    // this doesnt have much of a differece from loop insertion sort.
+    // a bit slower than loop verson of insertion sort, probably because of stack unwinding
+    // Slower than selection sort in this case too
+    const auto recursiveIns = testRecursiveInsertion(10);
+    cout<<endl << "Recursive insertion took "<<recursiveIns << " microseconds"<<endl;
+    // shuffle vector for another sorting round
+    cout << "Shuffling array ..."<<endl<<"Shuffled array";
+    shuffleVector(arr);
+    // print shuffled vector
+    printVector(arr);
+    cout << "Sorting using recursive insertion ..."<<endl<<"Sorted array";
+    // recursive insertion sorting
+    recursiveInsertion(arr,arr.size());
+    // print sorted vector
+    printVector(arr);
+    // selection sort
+    // selection sort is faster beacuse, althoough it has to compare more items,
+    // copying hapens only once per outer iteration;
+    // and since data here is int, which makes  comparing a faster task than copying a byte
+    const auto sel = testSelection(10);
+    cout <<endl<< "Selection sort took "<<sel <<" microseconds"<<endl;
+    // shuffle array again for another sorting
+    cout << "Shuffling array ..."<<endl<<"Shuffled array";
+    shuffleVector(arr);
+    // print vector
+    printVector(arr);
+    // selection sort
+    cout << "Selection sorting ..."<<endl<<"Sorted array";
+    selection(arr);
+    // print vector
+    printVector(arr);
 
-    sort.loopInsertion(arr);
-    sort.printVector(arr);
+
 }
--- UZ_4/sort.cpp
+++ UZ_4/sort.cpp
@@ -3,36 +3,58 @@
 //
 
 #include "sort.h"
-//#include <algorithm>
 #include <random>
 #include <iostream>
-
+# include <chrono>
+# include <algorithm>
 using namespace std;
 
 // insertion sort loop
-void Sort::loopInsertion(vector<int>& array) {
-    cout << "Insertion sorting ..."<<endl;
-    size_t i = 1;
-    while (i < array.size()){
-        int j = i;
-        while (j > 0 && (array[j-1] > array[i])){
+// sorts an array of elements in ascending order;
+// source pseudo code on wikipedia
+// [insertion sort](https://en.wikipedia.org/wiki/Insertion_sort)
+void loopInsertion(vector<int>& array) {
+    //cout << "Insertion sorting ..."<<endl;
+    // we start from second element
+    // because the first element is considered sorted
+    // as there are no items to compare it with
+    for(size_t i = 1; i < array.size();i++) {
+        // we compare item[i] with items below it,
+        // we keep swaping it until item to the left is smaller than it
+        size_t j = i;
+        while(j > 0 && array[j-1] > array[j]) {
+            // swap- see MACRO defined in header
             SWAP(array[j], array[j-1]);
             j--;
         }
-        array[j] = array[i];
-        i++;
     }
 }
 
 // insertion sort recursive
-void Sort::recursiveInsertion(vector<int>& array) {
+// [insertion sort](https://en.wikipedia.org/wiki/Insertion_sort)
+void recursiveInsertion(vector<int>& array, size_t n) {
 
+    if (n <= 1) { // last item, done sorting
+        // end
+        return;
+    }
+    // sort n-1 elements first
+    recursiveInsertion(array,n-1);
+    size_t j = n-1;
+    // swap positions to place item in to apropriate place
+    while(j > 0 && array[j-1] > array[j]) {
+        // swap- see MACRO defined in header
+        SWAP(array[j], array[j-1]);
+        j--;
+    }
 }
 
-// selection sort
-void Sort::selection(vector<int>& array)
+// selection sort, sorts elements one at a time,
+// find the smallest of none sorted items of the array,
+// and put it at apropriate place
+void selection(vector<int>& array)
 {
-    cout << "Selection sorting ..."<<endl;
+    //cout << "Selection sorting ..."<<endl;
     for(size_t i = 0; i< array.size()-1; i++){
         auto least = i;
         for(size_t j = i+1; j < array.size();j++){
@@ -40,42 +62,89 @@ void Sort::selection(vector<int>& array)
                 least = j;
             }
         }
-        if(i != least){
-            SWAP(array[i],array[least]);
+        if(i != least){ // no need to swap them if the items are equal -- optimizing
+            std::swap(array[i],array[least]);
         }
     }
 }
 
 // shuffles a vector so it can be sorted again
-void Sort::shuffleVector(vector<int>& array) {
-    cout << "shuffling vector..."<<endl;
+void shuffleVector(vector<int>& array) {
+    //cout << "shuffling vector..."<<endl;
     size_t size = array.size();
     for (size_t i = 0; i < size; i++)
     {
-        int r = rand() % size;
+        const size_t r = random() % size;
         SWAP(array[i], array[r]);
 
     }
 }
 
 // generates a vector of some size
-vector<int> Sort::generateArray(size_t size) {
-    int begin = 0 - (int)size/2; // so that we have negative and positive numbers
+vector<int> generateArray(const size_t size) {
+
     auto arr = vector<int>();
+    // populate array
     for(size_t i = 0; i < size; i++){
-        arr.push_back(random()% size);
-        begin++;
+
+        arr.push_back(static_cast<int>(random()% size)+1);
     }
-    // here we have a sorted array;
-    // so we shuffle it.
-    this->shuffleVector((arr));
+    //
     return arr;
 }
 
-void Sort::printVector(vector<int>& array) {
-    cout <<"Array elements = {"<< array[0];
-    for(size_t i = 0; i < array.size(); i++){
+void printVector(vector<int>& array) {
+    cout <<" = {"<< array[0];
+    for(size_t i = 1; i < array.size(); i++){
         cout <<  ", "<<array[i] ;
     }
     cout << " }"<<endl;
+}
+
+
+// testing functions
+unsigned long testInsertion(const int repeat){
+    unsigned long duration = 0;
+    for(int i = 0; i < repeat; i++) {
+        vector<int> arry = generateArray(8000);
+        // get start time
+        const auto start = std::chrono::high_resolution_clock::now().time_since_epoch();
+        loopInsertion(arry);
+        // get end time
+        const auto end = std::chrono::high_resolution_clock::now().time_since_epoch();
+        duration+= std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
+    }
+    return duration/repeat;
+
+}
+
+unsigned long testRecursiveInsertion(const int repeat){
+
+    
+    unsigned long duration = 0;
+    for(int i = 0; i < repeat; i++) {
+        vector<int> arry = generateArray(8000);
+        // get start time
+        const auto start = std::chrono::high_resolution_clock::now().time_since_epoch();
+        recursiveInsertion(arry, arry.size());
+        // get end time
+        const auto end = std::chrono::high_resolution_clock::now().time_since_epoch();
+        duration+= std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
+    }
+    return duration/repeat;
+
+}
+
+unsigned long testSelection(const int repeat) {
+    unsigned long duration = 0;
+    for(int i = 0; i < repeat; i++) {
+        vector<int> arry = generateArray(8000);
+        // get start time
+        const auto start = std::chrono::high_resolution_clock::now().time_since_epoch();
+        selection(arry);
+        // get end time
+        const auto end = std::chrono::high_resolution_clock::now().time_since_epoch();
+        duration+= std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
+    }
+    return duration/repeat;
 }
